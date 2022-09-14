4fe4680575f60624294fa50797c6fb6f69cf248f
chhak0503 <chhak0503@gmail.com>
2022-09-14 10:43:25 +0900

    2022/09/14 수업예제

--- Ch11/src/sub2/DeleteTest.java
+++ Ch11/src/sub2/DeleteTest.java
@@ -4,30 +4,29 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
-public class UpdateTest {
-	
+/*
+ * 날짜 : 2022/09/14
+ * 이름 : 김철학
+ * 내용 : JDBC Delete 실습하기
+ */
+public class DeleteTest {
 	public static void main(String[] args) {
+		
 		// DB정보
 		String host = "jdbc:mysql://127.0.0.1:3306/java1db";
 		String user = "root";
 		String pass = "1234";
-				
+		
 		try {
-			// 1단계 - JDBC 드라이버 로드
+			// 1단계
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			
-			// 2단계 - 데이터베이스 접속
+			// 2단계
 			Connection conn = DriverManager.getConnection(host, user, pass);
-			
-			// 3단계 - SQL 실행객체 생성
+			// 3단계
 			Statement stmt = conn.createStatement();
-			
 			// 4단계
-			String sql = "UPDATE `User1` SET `name`='홍길동', `hp`='010-1111-1111' ";
-				   sql += "WHERE `uid`='a101';";
-				   
+			String sql = "DELETE FROM `User1` WHERE `uid`='a101';";
 			stmt.executeUpdate(sql);
-			
 			// 5단계
 			// 6단계
 			stmt.close();
@@ -36,6 +35,6 @@ public class UpdateTest {
 			e.printStackTrace();
 		}
 		
-		System.out.println("Update 완료...");
+		System.out.println("Delete 완료...");
 	}
 }
