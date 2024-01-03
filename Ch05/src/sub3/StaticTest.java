3b401ab2b0d4444af5eb4ac5afc03f5f09e5bd05
chhak0503 <chhak0503@gmail.com>
2024-01-03 10:53:59 +0900

    상속예제 추가

--- Ch05/src/sub3/StaticTest.java
+++ Ch05/src/sub3/StaticTest.java
@@ -1,5 +1,7 @@
 package sub3;
 
+import sub4.StockAccount;
+
 /*
  * 날짜 : 2024/01/02
  * 이름 : 김철학
@@ -66,6 +68,8 @@ public class StaticTest {
 		System.out.println("rs2 : " + rs2);
 		System.out.println("rs3 : " + rs3);
 		System.out.println("rs4 : " + rs4);
+		
+		
 	}
 	
 }
