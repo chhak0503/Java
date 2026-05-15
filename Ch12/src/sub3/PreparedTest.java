496ca7b237b5d7a048b47d3a9e315e86ac1a11cb
chhak0503 <chhak0503@gmail.com>
2026-05-15 10:52:00 +0900

    add ch12

--- Ch12/src/sub3/PreparedTest.java
+++ Ch12/src/sub3/PreparedTest.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * 이름 : 김철학
  * 내용 : Java PreparedStatement 실습
  */
-public class PreparedInsertTest {
+public class PreparedTest {
 
 	public static void main(String[] args) {
 		
@@ -26,7 +26,30 @@ public class PreparedInsertTest {
 				exit = true;
 				break;
 			case 1:
-				System.out.println("입력 작업...");
+				System.out.println("입력 작업 시작!!!");
+				
+				System.out.print("아이디 입력 : ");
+				String userid = sc.next();
+				
+				System.out.print("이름 입력 : ");
+				String name = sc.next();
+				
+				System.out.print("휴대폰 입력 : ");
+				String hp = sc.next();
+				
+				System.out.print("나이 입력 : ");
+				int age = sc.nextInt();
+				
+				User1DTO dto = new User1DTO();
+				dto.setUserid(userid);
+				dto.setName(name);
+				dto.setHp(hp);
+				dto.setAge(age);
+				
+				// 데이터베이스 저장
+				User1DAO.insert(dto);
+				
+				System.out.println("등록 성공!!!");
 				break;
 			case 2:
 				System.out.println("조회 작업...");
